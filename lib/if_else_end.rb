1c4cd34f1f6853e5a9a7d96e28edf8bddd3011b3
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-07 19:19:04 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -6,12 +6,11 @@ end
 Odd!
  => nil
  
- 2.6.1 :031 > Time.now.to_i
- => 1604700282
-2.6.1 :032 > if Time.now.to_i.even?
-2.6.1 :033?>   puts "Even!"
-2.6.1 :034?>   else
-2.6.1 :035?>   puts "Odd!"
-2.6.1 :036?>   end
+Time.now.to_i
+ if Time.now.to_i.even?
+   puts "Even!"
+  else
+puts "Odd!"
+ end
 Odd!
  => nil
